4532212bc4fef5deb076f83437343c53ae92b87a
tamasvetstein <h986080@stud.u-szeged.hu>
2022-06-09 19:23:31 +0200

    https://github.com/tamasvetstein/szakdolgozat.git

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAxSGSXcpqZL9UrCkplkPDlh0VLvBHtOCY",
+    authDomain: "angular-szakd.firebaseapp.com",
+    projectId: "angular-szakd",
+    storageBucket: "angular-szakd.appspot.com",
+    messagingSenderId: "306825402132",
+    appId: "1:306825402132:web:9d61e9ec0e17d5c8cbd401"
+  }
 };
